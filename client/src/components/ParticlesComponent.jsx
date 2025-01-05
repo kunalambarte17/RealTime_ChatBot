033ad49019f976eeb3bf23bda456287673413abe
kunalambarte17 <ambartekunal17@gmail.com>
2025-01-05 11:14:32 +0530

    Styling Updated

--- client/src/components/ParticlesComponent.jsx
+++ client/src/components/ParticlesComponent.jsx
@@ -1,25 +1,14 @@
 import Particles, { initParticlesEngine } from "@tsparticles/react";
 import { useEffect, useMemo, useState } from "react";
-// import { loadAll } from "@/tsparticles/all"; // if you are going to use `loadAll`, install the "@tsparticles/all" package too.
-// import { loadFull } from "tsparticles"; // if you are going to use `loadFull`, install the "tsparticles" package too.
-import { loadSlim } from "@tsparticles/slim"; // if you are going to use `loadSlim`, install the "@tsparticles/slim" package too.
-// import { loadBasic } from "@tsparticles/basic"; // if you are going to use `loadBasic`, install the "@tsparticles/basic" package too.
-
+import { loadSlim } from "@tsparticles/slim"; 
 
 
 const ParticlesComponent = (props) => {
 
   const [init, setInit] = useState(false);
-  // this should be run only once per application lifetime
   useEffect(() => {
     initParticlesEngine(async (engine) => {
-      // you can initiate the tsParticles instance (engine) here, adding custom shapes or presets
-      // this loads the tsparticles package bundle, it's the easiest method for getting everything ready
-      // starting from v2 you can add only the features you need reducing the bundle size
-      //await loadAll(engine);
-      //await loadFull(engine);
       await loadSlim(engine);
-      //await loadBasic(engine);
     }).then(() => {
       setInit(true);
     });
@@ -34,7 +23,7 @@ const ParticlesComponent = (props) => {
     () => ({
       background: {
         color: {
-          value: "#1E2F97",
+          value: "#0d1b2a",
         },
       },
       fpsLimit: 120,
@@ -45,19 +34,19 @@ const ParticlesComponent = (props) => {
             mode: "push",
           },
           onHover: {
-            enable: true,
+            enable: false,
             mode: 'grab',
           },
         },
-        modes: {
-          push: {
-            distance: 200,
-            duration: 15,
-          },
-          grab: {
-            distance: 150,
-          },
-        },
+        // modes: {
+        //   push: {
+        //     distance: 200,
+        //     duration: 15,
+        //   },
+        //   grab: {
+        //     distance: 150,
+        //   },
+        // },
       },
       particles: {
         color: {
@@ -84,13 +73,13 @@ const ParticlesComponent = (props) => {
           density: {
             enable: true,
           },
-          value: 150,
+          value: 300,
         },
         opacity: {
           value: 1.0,
         },
         shape: {
-          type: "circle",
+          type: "square",
         },
         size: {
           value: { min: 1, max: 3 },
