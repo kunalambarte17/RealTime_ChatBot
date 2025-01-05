5c93d219b759cd639a59ec71139bed72a25d77de
kunalambarte17 <ambartekunal17@gmail.com>
2025-01-05 14:01:58 +0530

    update index.js

--- server/index.js
+++ server/index.js
@@ -10,8 +10,9 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
     cors:{
-        origin:"http://localhost:5173",
+        origin:"https://real-time-chat-bot.vercel.app/",
         methods:["GET", "POST"],
+        credentials: true,
     }
 })
 
